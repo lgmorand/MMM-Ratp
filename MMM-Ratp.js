c8d332e49d772b2e714b157e1c7dd9375f76d8a4
lgmorand <lgm@lgmorand.com>
2016-09-17 17:59:28 +0200

    v 1.0.0: stable and working version

--- MMM-Ratp.js
+++ MMM-Ratp.js
@@ -1,5 +1,4 @@
 /* Timetable for Paris local transport Module */
-
 /* Magic Mirror
  * Module: MMM-Ratp
  *
@@ -7,92 +6,91 @@
  * based on a script from Benjamin Angst http://www.beny.ch and Georg Peters (https://lane6.de)
  * MIT Licensed.
  */
- 
-Module.register("MMM-Ratp",{
+Module.register("MMM-Ratp", {
 
-	transports:[],
-	lineInfo:"",
+    transports: [],
+    lineInfo: "",
 
-	// Define module defaults
-	defaults: {
-		useRealtime: true,
-		updateInterval: 1 * 30 * 1000, // Update 30 secs
-		animationSpeed: 2000,
-		debugging: true,
-		retryDelay: 1 * 10 * 1000,
+    // Define module defaults
+    defaults: {
+        useRealtime: true,
+        updateInterval: 1 * 30 * 1000, // Update 30 secs
+        animationSpeed: 2000,
+        debugging: true,
+        retryDelay: 1 * 10 * 1000,
         initialLoadDelay: 0, // start delay seconds.
-	},
+    },
 
-	// Define required scripts.
-	getStyles: function() {
-		return [this.file("css/MMM-Ratp.css")];
-	},
+    // Define required scripts.
+    getStyles: function() {
+        return [this.file("css/MMM-Ratp.css")];
+    },
 
-	// Define start sequence.
-	start: function() {
-		Log.info("Starting module: " + this.name);
-		if(this.config.debugging) Log.info("DEBUG mode activated");
-		this.sendSocketNotification('CONFIG', this.config);
-		this.loaded = false;
-		this.updateTimer = null;
-	},    
-    
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
+    // Define start sequence.
+    start: function() {
+        Log.info("Starting module: " + this.name);
+        if (this.config.debugging) Log.info("DEBUG mode activated");
+        this.sendSocketNotification('CONFIG', this.config);
+        this.loaded = false;
+        this.updateTimer = null;
+    },
 
-		if (this.config.apiURL === "") {
-			wrapper.innerHTML = "Please set the correct API URL in the config of: " + this.name + ".";
-			wrapper.className = "dimmed light small ratptransport red";
-			return wrapper;
-		}
+    // Override dom generator.
+    getDom: function() {
+        var wrapper = document.createElement("div");
 
-		if (!this.loaded) {
-			wrapper.innerHTML = "Loading connections ...";
-			wrapper.className = "dimmed light small";
-			return wrapper;
-		}
+        if (this.config.apiURL === "") {
+            wrapper.innerHTML = "Please set the correct API URL in the config of: " + this.name + ".";
+            wrapper.className = "dimmed light small ratptransport red";
+            return wrapper;
+        }
 
-		var table = document.createElement("table");
-		table.className ="small";
+        if (!this.loaded) {
+            wrapper.innerHTML = "Loading connections ...";
+            wrapper.className = "dimmed light small";
+            return wrapper;
+        }
 
-		// creating title of the timetable
-		var rowtitle = document.createElement("th");
-		var title = document.createElement("td");
-		title.innerHTML = this.lineInfo;
-		rowtitle.appendChild(title);
-		table.appendChild(rowtitle);
+        var table = document.createElement("table");
+        table.className = "small";
 
-		// adding next schedules
-		for (var t in this.transports) {
-			var transports = this.transports[t];
+        // creating title of the timetable
+        var rowtitle = document.createElement("th");
+        var title = document.createElement("td");
+        title.innerHTML = this.lineInfo;
+        rowtitle.appendChild(title);
+        table.appendChild(rowtitle);
 
-			var row = document.createElement("tr");
+        // adding next schedules
+        for (var t in this.transports) {
+            var transports = this.transports[t];
 
-			var transportTimeCell = document.createElement("td");
-			transportTimeCell.innerHTML = transports.time;
-			transportTimeCell.className = "align-right bright";
-			row.appendChild(transportTimeCell);
+            var row = document.createElement("tr");
 
-			table.appendChild(row);
-		}
+            var transportTimeCell = document.createElement("td");
+            transportTimeCell.innerHTML = transports.time;
+            transportTimeCell.className = "align-right bright";
+            row.appendChild(transportTimeCell);
 
-		return table;
-	},
+            table.appendChild(row);
+        }
 
-	// using the results retrieved for the API call
-	socketNotificationReceived: function(notification, payload) {
-		Log.info("Notif:" + notification);
-		if (notification === "TRANSPORTS"){
-			if(this.config.debugging) {			
-				Log.info("Transports arrived");
-				Log.info(payload.lineInfo);
-				Log.info(payload.transports);
-			}
-			this.transports = payload.transports;
-			this.lineInfo = payload.lineInfo;
-			this.loaded = true;
-			this.updateDom(this.config.animationSpeed);
-		}
-	}
+        return table;
+    },
+
+    // using the results retrieved for the API call
+    socketNotificationReceived: function(notification, payload) {
+        Log.info("Notif:" + notification);
+        if (notification === "TRANSPORTS") {
+            if (this.config.debugging) {
+                Log.info("Transports arrived");
+                Log.info(payload.lineInfo);
+                Log.info(payload.transports);
+            }
+            this.transports = payload.transports;
+            this.lineInfo = payload.lineInfo;
+            this.loaded = true;
+            this.updateDom(this.config.animationSpeed);
+        }
+    }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,4 @@
 /* Timetable for Paris local transport Module */
-
 /* Magic Mirror
  * Module: MMM-Ratp
  *
@@ -7,119 +6,125 @@
  * based on a script from Benjamin Angst http://www.beny.ch and Georg Peters (https://lane6.de)
  * MIT Licensed.
  */
-
 const NodeHelper = require("node_helper");
 const forge = require('node-forge');
 const unirest = require('unirest');
 
 module.exports = NodeHelper.create({
 
-		start: function () {
-	    		this.started = false;
-			console.log("NodeH started");
-	  	},
+    updateTimer: "",
+    start: function() {
+        this.started = false;
+        console.log("MMM-Ratp- NodeHelper started");
+    },
 
-	  
 
-		/* updateTimetable(transports)
-		 * Calls processTransports on succesfull response.
-		 */
-		updateTimetable: function() {
-			var url = this.config.apiURL;
-			var self = this;
-			var retry = false;
 
-			if(this.config.debugging) {
-				console.log("Function updateTimeTable");
-			}
+    /* updateTimetable(transports)
+     * Calls processTransports on succesfull response.
+     */
+    updateTimetable: function() {
+        var url = this.config.apiURL;
+        var self = this;
+        var retry = false;
 
-			// calling this API
-		    unirest.get(url)
-		    .end(function (r) {
-		    	if (r.error) {
-		    		console.log(self.name + " : " + r.error);
-		    		retry = true;
-		    	}
-		    	else {
-					if(this.config.debugging) console.log("body: ", JSON.stringify(r.body));
-		    		self.processTransports(r.body);
-		    	}
 
-		    	if (retry) {
-					if(this.config.debugging) self.scheduleUpdate((self.loaded) ? -1 : this.config.retryDelay);
-				}
-		    });
-		},
-		// Help to retrieve a type which can be directly displayed
-		getSanitizedName:function(type)
-		{
-			var t= "";
-				switch(type)
-				{
-					case "bus": t= "Bus"; break; 
-					case "rers": t= "RER";break; 
-					case "tramways": t= "Tramway";break; 
-					case "noctiliens": t= "Noctilien";break; 
-					case "metros": t= "Metro";break; 
-					default: t= "";
-				}
+        // calling this API
+        unirest.get(url)
+            .end(function(r) {
+                if (r.error) {
+                    console.log(self.name + " : " + r.error);
+                    retry = true;
+                } else {
+                    self.processTransports(r.body);
+                }
 
-			return t;
-		},
+                if (retry) {
+                    console.log("retrying");
+                    self.scheduleUpdate((self.loaded) ? -1 : this.config.retryDelay);
+                }
+            });
+    },
+    // Help to retrieve a type which can be directly displayed
+    getSanitizedName: function(type) {
+        var t = "";
+        switch (type) {
+            case "bus":
+                t = "Bus";
+                break;
+            case "rers":
+                t = "RER";
+                break;
+            case "tramways":
+                t = "Tramway";
+                break;
+            case "noctiliens":
+                t = "Noctilien";
+                break;
+            case "metros":
+                t = "Metro";
+                break;
+            default:
+                t = "";
+        }
 
-		/* processTransports(data)
-		 * Uses the received data to set the various values.
-		 */
-		processTransports: function(data) {
-			if(this.config.debugging) {
-				console.log("Processing transports:" + data);
-			}
+        return t;
+    },
 
-			this.transports = [];
-		
-		    	this.lineInfo =  data.response.informations.type + " " + data.response.informations.line + " (vers " + data.response.informations.destination.name + ")";
-			for (var i = 0, count = data.response.schedules.length; i < count; i++) {
+    /* processTransports(data)
+     * Uses the received data to set the various values.
+     */
+    processTransports: function(data) {
 
-				var nextTransport = data.response.schedules[i];
+        this.transports = [];
 
-				this.transports.push({
-					name: nextTransport.destination,
-					time: nextTransport.message
-				});
-			}
-			this.loaded = true;
-			this.sendSocketNotification("TRANSPORTS", {transports:this.transports, lineInfo: this.lineInfo});
-		},
-		
+        this.lineInfo = this.getSanitizedName(data.response.informations.type) + " " + data.response.informations.line + " (vers " + data.response.informations.destination.name + ")";
+        for (var i = 0, count = data.response.schedules.length; i < count; i++) {
 
-		/* scheduleUpdate()
-		 * Schedule next update.
-		 * argument delay number - Millis econds before next update. If empty, this.config.updateInterval is used.
-		 */
-		scheduleUpdate: function(delay) {
-			var nextLoad = this.config.updateInterval;
-			if (typeof delay !== "undefined" && delay >= 0) {
-				nextLoad = delay;
-			}
+            var nextTransport = data.response.schedules[i];
 
-			var self = this;
-			clearTimeout(this.updateTimer);
-			this.updateTimer = setTimeout(function() {
-				self.updateTimetable();
-			}, nextLoad);
-		},
+            this.transports.push({
+                name: nextTransport.destination,
+                time: nextTransport.message
+            });
+        }
+        this.loaded = true;
+        this.sendSocketNotification("TRANSPORTS", {
+            transports: this.transports,
+            lineInfo: this.lineInfo
+        });
+    },
 
-		socketNotificationReceived: function(notification, payload) {
-			console.log("Notif received: " + notification);
-			const self = this;
-		  	if (notification === 'CONFIG' && this.started == false) {
-		    	this.config = payload;
-		    	this.started = true;
-		    	self.scheduleUpdate(this.config.initialLoadDelay);
-		    }
-			if(this.config.debugging) {
-				console.log("Notif received: " + notification);
-				console.log(payload);
-			}
-		}
+
+    /* scheduleUpdate()
+     * Schedule next update.
+     * argument delay number - Millis econds before next update. If empty, this.config.updateInterval is used.
+     */
+    scheduleUpdate: function(delay) {
+        var nextLoad = this.config.updateInterval;
+
+        if (typeof delay !== "undefined" && delay > 0) {
+            nextLoad = delay;
+        }
+
+        var self = this;
+        clearTimeout(this.updateTimer);
+        this.updateTimer = setInterval(function() {
+            self.updateTimetable();
+        }, nextLoad);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (payload.debugging) {
+            console.log("Notif received: " + notification);
+            console.log(payload);
+        }
+
+        const self = this;
+        if (notification === 'CONFIG' && this.started == false) {
+            this.config = payload;
+            this.started = true;
+            self.scheduleUpdate(this.config.initialLoadDelay);
+        }
+    }
 });
